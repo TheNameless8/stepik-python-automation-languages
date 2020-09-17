8101865612a18a32a775ce95f87051734974fa0e
TheNameless8 <38660458+TheNameless8@users.noreply.github.com>
2020-09-17 23:06:43 +0400

    Изменен метод поиска кнопки добавления в корзину

--- test_items.py
+++ test_items.py
@@ -5,8 +5,8 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_product_page_have_add_button(browser):
     browser.get(link)
     time.sleep(10)
-    basket_button_count = browser.find_elements_by_class_name("btn-add-to-basket")
-    #assert basket_button, "Should be a button"
-    assert len(basket_button_count) == 1, "Нет кнопки добавления в корзину"
+    basket_button = browser.find_elements_by_class_name("btn-add-to-basket")
+    assert basket_button, "Нет кнопки добавления в корзину"
+    #assert len(basket_button_count) == 1, "Нет кнопки добавления в корзину"
 
 
